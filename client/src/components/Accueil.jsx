543bc4efdb5951df853c1d0c4f65964b8f96a9c4
CHRISTOPHE BOINET <boinet.c19@free.fr>
2022-05-19 18:32:21 +0200

    A propos

--- client/src/components/Accueil.jsx
+++ client/src/components/Accueil.jsx
@@ -9,10 +9,13 @@ export default function Accueil() {
     <div>
       <section className="accueil_container">
         <div className="accueil_present">
-          <h1>Christophe BOINET, Développeur Web</h1>
-          <h2>Je suis à l'écoute d'opportunités en développement de sites et applications web.</h2>
+          <div className="accueil_intro">
+            <h1>Christophe BOINET</h1>
+            <h2>Développeur Web</h2>
+            <h3>Acuellement en formation intensive à la Wild Code School et à la recherche d'une alternance pour 12 mois</h3>
+          </div>
+          <img src={photo} className="moi" alt="moi" />
         </div>
-        <img src={photo} className="moi" alt="moi" />
         <div className="action_button">
           <button type="button" className="action">Consulter mon CV</button>
           <button type="button" className="action">Me contacter</button>
--- client/src/components/Apropos.jsx
+++ client/src/components/Apropos.jsx
@@ -5,22 +5,61 @@ export default function Apropos() {
   return (
     <section className="apropos_container">
       <div className="apropos_present">
-        <p id="#apropos_anchor">Qui suis-je ?</p>
-
+        <h1>Qui suis-je ?</h1>
+        <p>Après plus de 20 ans dans le management de la Qualité et une expérience comme thérapeute, j'ai décidé d’entreprendre une formation avec la Wild Code
+          School, pour devenir Développeur Web.</p>
+        <p>💡 J'ai à cœur d'apprendre et de comprendre grâce aux nombreux défis qui m'attendent. 💡</p>
       </div>
+
       <div className="skills_hard">
-        <p>Langages et technologies utilisés</p>
-
+        <h1>Langages et technologies utilisés</h1>
+        <div className="skills">
+          <p>HTML5</p>
+          <img />
+        </div>
+        <div className="skills">
+          <p>CSS3</p>
+          <img />
+        </div>
+        <div className="skills">
+          <p>Javascript</p>
+          <img />
+        </div>  
+        <div className="skills">
+          <p>React.js</p>
+          <img />
+        </div> 
+        <div className="skills">
+          <p>Node.js/Express</p>
+          <img />
+        </div> 
+        <div className="skills">
+          <p>Visual Studio Code</p>
+          <img />
+        </div> 
+        <div className="skills">
+          <p>GitHub</p>
+          <img />
+        </div> 
       </div>
+
       <div className="skills_soft">
-        <p>Qualités et compétences</p>
-
+        <h1>Qualités et compétences</h1>
+        <div className="skills">
+          <p>Orientation client</p>
+          <p>Adaptation</p>
+          <p>Gestion de projets</p>
+          <p>Engagement dans les projets</p>
+        </div>
       </div>
-      <div className="apropos_job">
-        <p>CV</p>
 
+      <div className="apropos_cv">
+        <h1>CV</h1>
+        <div className="action_button">
+          <button type="button" className="action">Consulter mon CV</button>
+          <button type="button" className="action">Me contacter</button>
+        </div>
       </div>
-
     </section>
 
   );
